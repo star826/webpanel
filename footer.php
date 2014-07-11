2e47b9ab0260b9a61e37b4ded3f783f4491b9054
star826 <myniejianping@Hotmail.com>
2014-07-11 21:43:55 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,15 +4,9 @@ if (empty($page) && empty($adminpage)) ForceDie();
 ForceLogin();
 ?>
 <br />
-<p style="color:#000000;font-size:14px;">&copy; 2014 Web Control Panel For <a href="http://www.lnmpv.com">LNMPV</a> Processed in <?php echo round(get_microtime()-$page_start_time,6); ?> second(s) <br /><br />
+<p style="color:#000000;font-size:14px;">&copy; 2014 Web Control Panel For <a href="http://www.lnmpv.com">LNMPV</a> All rights reserved. Processed in <?php echo round(get_microtime()-$page_start_time,6); ?> second(s) <br /><br />
 <?php if (IsAdmin()) { ?>
- <?php
-if (empty($adminpage)){
-	echo '<a href="admin_system.php" style="color:#FF0000;font-size:14px;">转到-'.$lang['admin_panel'].'</a>';
-} else {
-	echo '<a href="vhost.php" style="color:#FF0000;font-size:14px;">转到-'.$lang['user_panel'].'</a>';
-}
-?></p>
+</p>
 <?php } ?>
 <?php echo $statisticalCode; ?>
 </body>
