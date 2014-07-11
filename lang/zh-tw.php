e8280ef4a2a2993f1d3bf73886c9133247b55d6e
Jason Ho <eflyjason@gmail.com>
2014-07-11 22:23:28 +0800

    Add zh-TW Translation

---
+++ lang/zh-tw.php
@@ -0,0 +1,280 @@
+<?php
+// Modify ：arefly (eflyjason@gmail.com)
+// Date ：2014 / 07 / 11
+// access_denied.php
+$lang['access_denied'] = '訪問被拒絕';
+$lang['access_denied_text'] = '很抱歉，你沒有足夠的權限訪問這個對像或執行此操作。';
+$lang['access_denied_hint'] = '如果你認為這是錯誤的，請 <a href="support_ticket_new.php">提交Ticket</a> 聯系技術支持。';
+
+// login.php || register.php || forgot_username.php || forgot_password.php
+
+$lang['username'] = '使用者名稱';
+$lang['password'] = '密碼';
+$lang['input_username'] = '請輸入您的使用者名稱！';
+$lang['input_password'] = '請輸入您的帳戶密碼！';
+$lang['login'] = '登錄';
+$lang['register'] = '注冊';
+$lang['cookies_must_be_enabled'] = '必須啟用Cookie';
+$lang['forgot_username'] = '忘記使用者名稱';
+$lang['forgot_password'] = '忘記密碼';
+$lang['forgot_username_hint'] = '請輸入您帳戶的電子信箱地址。 我們將會通過郵件通知你任何匹配的用戶名。';
+$lang['forgot_password_hint'] = '請提供您的使用者名稱。我們將向您發送一封電子郵件，其中包含進一步的說明。';
+$lang['input_email'] = '請輸入您的郵箱地址.';
+$lang['submit'] = '提交';
+$lang['back_to_login'] = '返回登陸';
+
+
+ 
+// header.php
+$lang['my_profile'] = '我的信息';
+$lang['log_out'] = '注銷';
+$lang['admin_panel'] = '管理員面板';
+$lang['user_panel'] = '用戶面板';
+$lang['vhost'] = '虛擬主機';
+$lang['vhosts'] = '虛擬主機';
+$lang['ssh'] = 'SSH';
+$lang['cdn'] = 'CDN';
+$lang['dns'] = 'DNS';
+$lang['users'] = '使用者';
+$lang['billing'] = '賬單';
+$lang['statistics'] = '統計';
+$lang['vps'] = 'VPS';
+$lang['account'] = '帳戶';
+$lang['support'] = '支持';
+$lang['tools'] = '工具';
+$lang['blog'] = '官方部落格';
+
+// profile.php
+
+$lang['language'] = '語言';
+$lang['save_language_setting'] = '儲存語言設置';
+$lang['theme'] = '主題';
+$lang['save_theme_setting'] = '儲存主題設置';
+$lang['change_password'] = '更改密碼';
+$lang['new_password'] = '新密碼';
+$lang['email'] = '郵箱';
+$lang['current_email'] = '當前郵箱';
+$lang['new_email'] = '新郵箱';
+$lang['change_email'] = '更改郵箱';
+$lang['referrals'] = '推介';
+$lang['description'] = '說明';
+$lang['your_referral_url'] = '您的推廣鏈接';
+$lang['api_key'] = 'API 密鑰';
+$lang['generate_a_new_api_password'] = '生成一個新的API密鑰';
+
+
+// account_subtabs.php
+
+$lang['account'] = '帳戶';
+$lang['contact_info'] = '聯系';
+$lang['make_a_payment'] = '充值';
+$lang['billing_history'] = '賬單';
+
+// account.php
+
+$lang['no_contact_details'] = '您還未填寫詳細的聯系方式，請盡快補全您的聯系信息。';
+$lang['account_information'] = '帳戶信息';
+$lang['contact'] = '聯系';
+$lang['address'] = '地址';
+$lang['email_addr'] = '郵箱地址';
+$lang['security'] = '安全';
+$lang['last_login_ip'] = '上次登錄IP';
+$lang['last_login_time'] = '上次登錄時間';
+$lang['current_login_ip'] = '本地登錄IP';
+$lang['current_login_time'] = '本地登錄時間';
+$lang['recent_billing_activity_and_account_balance'] = '最近帳單活動和帳戶余額';
+$lang['recent_activity'] = '最近的活動';
+$lang['undone'] = '未完成';
+$lang['unpaid'] = '未支付';
+$lang['current_balance'] = '目前余額';
+$lang['currency_symbol'] = ' 元';
+
+// account_contact.php
+
+$lang['contact_information'] = '聯絡信息';
+$lang['company_name'] = '公司名稱';
+$lang['Email'] = '郵箱地址';
+$lang['first_ame'] = '名';
+$lang['last_name'] = '姓';
+$lang['Address1'] = '地址1';
+$lang['Address2'] = '地址2';
+$lang['city'] = '市';
+$lang['State'] = '省';
+$lang['Zip'] = '郵編';
+$lang['Country'] = '國家';
+$lang['Phone1'] = '電話1';
+$lang['Phone2'] = '電話2';
+$lang['qq'] = 'QQ';
+$lang['save_changes'] = '儲存更改';
+
+// dns
+$lang['domain_zone'] = '域名';
+$lang['type'] = '類型';
+$lang['last_modified'] = '上次修改';
+$lang['status'] = '統計';
+$lang['options'] = '選項';
+$lang['add_a_domain_zone'] = '添加一個域名';
+$lang['active'] = '活躍';
+$lang['disabled'] = '禁用';
+$lang['edit_mode'] = '編輯模式';
+$lang['edit'] = '編輯';
+$lang['remove'] = '刪除';
+$lang['check'] = '檢查';
+$lang['soa_record'] = 'SOA記錄';
+$lang['primary_dns'] = '主DNS';
+$lang['default_ttl'] = '默認TTL';
+$lang['refresh_rate'] = '重新整理率';
+$lang['retry_rate'] = '重試率';
+$lang['expire_time'] = '過期時間';
+$lang['settings'] = '設置';
+$lang['ns_records'] = 'NS記錄';
+$lang['name_server'] = '名稱伺服器';
+$lang['subdomain'] = '子域名';
+$lang['ttl'] = 'TTL';
+$lang['add_a_new_ns_record'] = '添加一個新的NS記錄';
+$lang['mx_records'] = 'MX記錄';
+$lang['mail_server'] = '郵件伺服器';
+$lang['preference'] = '優先順序';
+$lang['add_a_new_mx_record'] = '添加一個新的MX記錄';
+$lang['a_aaaa_records'] = 'A/AAAA記錄';
+$lang['hostname'] = '主機';
+$lang['ip_address'] = 'IP地址';
+$lang['add_a_new_a_record'] = '添加一個新的A記錄';
+$lang['cname_records'] = 'CNAME記錄';
+$lang['aliases_to'] = '別名';
+$lang['add_a_new_cname_record'] = '添加一個新的CNAME記錄';
+$lang['txt_records'] = 'TXT記錄';
+$lang['name'] = '名稱';
+$lang['value'] = '值';
+$lang['add_a_new_txt_record'] = '添加一個新的TXT記錄';
+$lang['srv_records'] = 'SRV記錄';
+$lang['service'] = '服務';
+$lang['domain'] = '域名';
+$lang['priority'] = '優先級';
+$lang['weight'] = '權重';
+$lang['port'] = '埠';
+$lang['target'] = '目標';
+$lang['add_a_new_srv_record'] = '添加一個新的SRV記錄';
+$lang['domain_note'] = '<strong>注：</strong>所進行的更改將在15分鐘後生效.';
+$lang['add_a_zone'] = '添加一個區域';
+$lang['add_a_master_zone'] = '添加一個主要的區域';
+$lang['soa_email'] = 'SOA郵箱';
+$lang['add_a_master_zone'] = '添加一個主區';
+$lang['abuse_note'] = '為防止濫用, 您需要有一個活動的虛擬主機或賬戶內余額大於10元.';
+$lang['enter_domain'] = '請輸入一個域名';
+$lang['enter_soa_email'] = '請輸入SOA郵箱';
+$lang['domain_add_success'] = '添加域名成功';
+$lang['domain_not_registered'] = '您的域名沒有被注冊';
+$lang['domain_not_correct'] = '您的域名不正確';
+$lang['check_zone'] = '檢查區域';
+$lang['domain_ns_settings_correct'] = '您的域名NS伺服器設置是正確的';
+$lang['setting_domain_ns_servers_note'] = '請設定您的域名NS伺服器為';
+$lang['success_deleted_domain'] = '刪除域名成功';
+$lang['remove_zone'] = '刪除區域';
+$lang['sure_delete_zone'] = '你確定是否刪除這個區域';
+$lang['yes_delete_this_sucker'] = '是的, 刪除這個';
+$lang['select_domain_status'] = '請選擇域名狀態';
+$lang['select_default_ssl'] = '請選擇默認TTL';
+$lang['select_refresh_rate'] = '請選擇Refresh Rate';
+$lang['select_retry_rate'] = '請選擇Retry Rate';
+$lang['select_expire_rate'] = '請選擇Expire Rate';
+$lang['update_soa_success'] = '更新SOA成功';
+$lang['edit_soa_record'] = '編輯SOA記錄';
+$lang['domain_settings_and_defaults'] = '域名設置和默認設置';
+$lang['domain_example'] = '例：ns1.domain.com';
+$lang['your_email_address'] = '您的郵箱地址';
+$lang['domain_status'] = '域名狀態';
+$lang['domain_status_active'] = '活動 - 打開該域名的活動';
+$lang['domain_status_disabled'] = '禁用 - 關掉該域名服務';
+$lang['domain_status_edit_mode'] = '編輯模式 - 使用這種模式，同時編輯';
+$lang['advanced_settings'] = '高級設置';
+$lang['default'] = '默認';
+$lang['default_ttl_hint'] = '定義在該區域的所有條目的默認生存時間';
+$lang['default_refresh_rate_hint'] = '確定二級/從域名伺服器檢查的頻率，與主更新';
+$lang['default_retry_rate_hint'] = '如果最後嘗試失敗的時間 二級/從域名伺服器將等待再次聯系主域名伺服器.';
+$lang['expire_rate'] = '到期';
+$lang['default_expire_rate_hint'] = '多久二級/從域名伺服器會才考慮它的DNS數據陳舊的等待，如果它可以\'到達主域名伺服器';
+$lang['create_change_record_success'] = '創建/更改記錄成功';
+$lang['edit_resource_record'] = '編輯資源記錄';
+$lang['add_edit_an_ns_record'] = '添加/編輯NS記錄';
+$lang['name_server_hint'] = '例：ns1.domain.com';
+$lang['subdomain_hint'] = '離開這個空白，除非你重新委派子域的域名伺服器上面';
+$lang['add_edit_an_mx_record'] = '添加/編輯MX記錄';
+$lang['mail_server_hint'] = '例：mail2.example.com';
+$lang['mx_subdomain_hint'] = '保留為空，除非委派子域的郵件伺服器上。通配符也是有效的在這裡.';
+$lang['add_edit_an_a_aaaa_record'] = '添加/編輯A/AAAA記錄';
+$lang['hostname_hint'] = '例：www.example.com';
+$lang['add_edit_an_cname_record'] = '添加/編輯CNAME記錄';
+$lang['cname_hostname_hint'] = '例：www.thisdomain.com';
+$lang['aliases_to_hint'] = '例：www.otherdomain.com';
+$lang['add_edit_an_txt_record'] = '添加/編輯TXT記錄';
+$lang['add_edit_an_srv_record'] = '添加/編輯SRV記錄';
+$lang['srv_service_hint'] = '例：_sip';
+$lang['protocol'] = '協議';
+$lang['protocol_tcp'] = 'tcp';
+$lang['protocol_udp'] = 'udp';
+$lang['protocol_xmpp'] = 'xmpp';
+$lang['protocol_tls'] = 'tls';
+$lang['delete_record_success'] = '刪除記錄成功';
+$lang['remove_record'] = '刪除記錄';
+$lang['sure_delete_record'] = '你確定要刪除這個記錄?';
+
+// support
+$lang['open_tickets'] = '打開的支持票';
+$lang['ticket_id'] = '編號';
+$lang['summary'] = '主題';
+$lang['status'] = '狀態';
+$lang['opened'] = '打開';
+$lang['closed'] = '關閉';
+$lang['last_updated'] = '最後更新';
+$lang['by'] = '由';
+$lang['open_a_new_support_ticket'] = '打開一個新的支持票';
+$lang['my_tickets'] = '我的支持票';
+$lang['all_tickets'] = '所有支持票';
+$lang['recently_closed_tickets'] = '最近關閉的支持票';
+$lang['none'] = '無';
+$lang['summary_description_regarding_required'] = '主題, 描述, 相關服務是需要填寫與選擇的, 請重試.';
+$lang['ticket_submit_success'] = '支持單已經提交.';
+$lang['support'] = '支持';
+$lang['open_a_ticket'] = '打開一個支持票';
+$lang['regarding'] = '相關服務';
+$lang['please_select'] = '請選擇';
+$lang['available'] = '可用';
+$lang['suspended'] = '停止';
+$lang['other_general_billing_etc'] = '其他 (銷售,財務,等)';
+$lang['description'] = '描述';
+$lang['open_ticket'] = '提交支持票';
+$lang['content_required'] = '請填寫回復內容.';
+$lang['ticket_has_been_closed'] = '支持票已經關閉.';
+$lang['close_success'] = '支持票關閉成功.';
+$lang['delete_success'] = '支持票刪除成功.';
+$lang['reply_close_success'] = '回復且關閉支持票成功.';
+$lang['reply_success'] = '回復成功';
+$lang['add_update'] = '增加更新';
+$lang['reply'] = '回復';
+$lang['reply_and_return'] = '回復並返回';
+$lang['reply_and_close'] = '回復並關閉';
+$lang['close'] = '關閉';
+$lang['delete'] = '刪除';
+
+
+// function.php datetime
+$lang['datetime_day'] = '天';
+$lang['datetime_hour'] = '小時';
+$lang['datetime_minute'] = '分鐘';
+$lang['datetime_second'] = '秒';
+$lang['datetime_plural'] = '';
+$lang['datetime_ago'] = '前';
+$lang['datetime_moment'] = '瞬間';
+$lang['datetime_now'] = '現在';
+
+
+// Linodes
+// Modify ：nightgod (davidng530@gmail.com)
+// Date ：2014 / 07 / 11
+$lang['linode'] = '主機';
+$lang['linodes'] = '主機節點';
+$lang['linodeplans'] = '主機計劃';
+$lang['datacenters'] = '數據中心';
+$lang['distributions'] = '分配';
+$lang['kernels'] = '內核';
