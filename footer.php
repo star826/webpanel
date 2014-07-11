a0d1c5b974ef7fb24194326ccdfe181b4609f37e
Jason Ho <eflyjason@gmail.com>
2014-07-11 22:18:29 +0800

    Fix Footer

--- footer.php
+++ footer.php
@@ -4,10 +4,7 @@ if (empty($page) && empty($adminpage)) ForceDie();
 ForceLogin();
 ?>
 <br />
-<p style="color:#000000;font-size:14px;">&copy; 2014 Web Control Panel For <a href="http://www.lnmpv.com">LNMPV</a> All rights reserved. Processed in <?php echo round(get_microtime()-$page_start_time,6); ?> second(s) <br /><br />
-<?php if (IsAdmin()) { ?>
-</p>
-<?php } ?>
+<p style="color:#000000;font-size:14px;">&copy; 2014 Web Control Panel For <a href="http://www.lnmpv.com">LNMPV</a> All rights reserved. Processed in <?php echo round(get_microtime()-$page_start_time,6); ?> second(s) <br /><br /></p>
 <?php echo $statisticalCode; ?>
 </body>
 </html>
